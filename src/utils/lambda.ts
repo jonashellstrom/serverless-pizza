4f0462dd657371c713b492b4a0be2e4bb540abc0
jonashellstrom <jonas.dev@icloud.com>
2021-07-10 10:02:13 -0400

    utils: lambda middy wrapper with JSON schema validator

--- src/utils/lambda.ts
+++ src/utils/lambda.ts
@@ -1,7 +1,27 @@
 import middy from "@middy/core"
 import middyJsonBodyParser from "@middy/http-json-body-parser"
 import { Handler } from "aws-lambda/handler"
+import validator from "@middy/validator"
 
 export const middyfy = (handler: Handler) => {
   return middy(handler).use(middyJsonBodyParser())
+}
+
+export const middyfyWithValidatorSchema = (
+  handler: Handler,
+  schema: { [key: string]: unknown }
+) => {
+  return middyfy(handler).use(
+    validator({
+      inputSchema: {
+        type: "object",
+        required: ["body"],
+        properties: {
+          body: {
+            ...schema,
+          },
+        },
+      },
+    })
+  )
 }
